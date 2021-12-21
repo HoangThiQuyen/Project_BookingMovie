d450ef4f1406094b5c31464562afca942bbef160
quyen <hoangquyen99@gmail.com>
2021-12-21 17:53:31 +0700

    updated 21/12

--- src/templates/HomeTemplate.js
+++ src/templates/HomeTemplate.js
@@ -1,21 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import NavbarUser from "./../components/Navbar";
 
 function HomeLayout(props) {
-  if (props.children.props.location.pathname === "/detail-ticketroom") {
-    return (
-      <div>
-        {props.children}
-      </div>
-    )
+  if (props?.children.props.location.pathname === "/detail-ticketroom") {
+    return <div>{props.children}</div>;
   } else {
     return (
       <div>
         <NavbarUser />
         {props.children}
       </div>
-    )
+    );
   }
 }
 export default function HomeTemplate({ Component, ...props }) {
@@ -25,23 +21,25 @@ export default function HomeTemplate({ Component, ...props }) {
        path*/
       {...props}
       render={(propsComponent) => {
-
-        if (propsComponent.match.path === "/" || propsComponent.match.path === "/detailMovie/:id") {
+        if (
+          propsComponent.match.path === "/" ||
+          propsComponent.match.path === "/detailMovie/:id"
+        ) {
           return (
             <HomeLayout>
               <Component {...propsComponent} />
             </HomeLayout>
-          )
+          );
         }
         if (localStorage.getItem("userHome")) {
           return (
             <HomeLayout>
               <Component {...propsComponent} />
             </HomeLayout>
-          )
+          );
         }
-        return <Redirect to="/auth-home" />
+        return <Redirect to="/auth-home" />;
       }}
     />
-  )
+  );
 }
